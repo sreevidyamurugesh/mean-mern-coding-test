d2ade2ac8d4a6f48f424322a68f0ed6fcf727829
Vidyasree <vidya.cse22@gmail.com>
2026-03-20 00:33:36 +0530

    Initial commit - Full stack coding test

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    //const uri = "mongodb+srv://vidya:iC3aEPP4vuvBrYlb@cluster0.srrwn8h.mongodb.net/?appName=Cluster0";
-    const uri = "mongodb://vidya:iC3aEPP4vuvBrYlb@ac-mxoxey1-shard-00-00.srrwn8h.mongodb.net:27017,ac-mxoxey1-shard-00-01.srrwn8h.mongodb.net:27017,ac-mxoxey1-shard-00-02.srrwn8h.mongodb.net:27017/?ssl=true&replicaSet=atlas-eu01qi-shard-0&authSource=admin&appName=Cluster0";
+    const uri = "mongodb://vidya:*******@ac-mxoxey1-shard-00-00.srrwn8h.mongodb.net:27017,ac-mxoxey1-shard-00-01.srrwn8h.mongodb.net:27017,ac-mxoxey1-shard-00-02.srrwn8h.mongodb.net:27017/?ssl=true&replicaSet=atlas-eu01qi-shard-0&authSource=admin&appName=Cluster0";
     await mongoose.connect(uri);
     console.log('MongoDB Connected ✅');
   } catch (error) {
